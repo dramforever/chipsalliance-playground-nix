136767b1a419b8d6971aba52a44f649c48603f05
dramforever <dramforever@live.com>
2021-07-31 11:09:25 +0800

    Use clang12Stdenv

--- env.nix
+++ env.nix
@@ -1,9 +1,9 @@
-{ mkShell, gcc11Stdenv
+{ mkShell, clang12Stdenv
 , git, mill, wget, parallel, dtc, protobuf, antlr
 , verilator, cmake, ninja
 }:
 
-mkShell {
+mkShell.override { stdenv = clang12Stdenv; } {
   name = "playground";
 
   depsBuildBuild = [ git mill wget parallel dtc protobuf antlr verilator cmake ninja ];
